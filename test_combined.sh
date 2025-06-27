8a74125fe4735e6b027c08995310e0f17d9f0337
Jakub Potempa <jpotempa@student.ethz.ch>
2025-06-27 17:24:37 +0200

    add seperate folder for bug-triggering programs

--- test_combined.sh
+++ test_combined.sh
@@ -5,15 +5,15 @@ currentProgram=0
 while true; do
     timeout 2 ./generator
     if ghc -o illtyped_haskell illtyped.hs | grep -q "\[2 of 2\]"; then # [2 of 2] is the string denoting successful compilation in GHC
-        cp expr_log.txt expr_log_bug_haskell$currentProgram.txt
-        cp illtyped.hs illtyped_bug_haskell$currentProgram.hs
+        cp expr_log.txt bugs/expr_log_bug_haskell$currentProgram.txt
+        cp illtyped.hs bugs/illtyped_bug_haskell$currentProgram.hs
         # break;
     fi
     ocamlc -o illtyped_ocaml illtyped.ml
     if [ $? -eq 0 ]; then # status code 0 denotes successful compilation
         if [ -s illtyped.ml ]; then # only copy the log and program files if the program is not empty
-            cp expr_log.txt expr_log_bug_ocaml$currentProgram.txt
-            cp illtyped.ml illtyped_bug_ocaml$currentProgram.ml
+            cp expr_log.txt bugs/expr_log_bug_ocaml$currentProgram.txt
+            cp illtyped.ml bugs/illtyped_bug_ocaml$currentProgram.ml
         fi
     fi
     currentProgram=$((currentProgram + 1))
